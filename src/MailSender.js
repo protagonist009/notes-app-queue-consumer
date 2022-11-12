e625d542ae64f3626d2e9dd5dc5c18e117ce320a
lintang-b-s <lintangbirdasaputra@gmail.com>
2022-11-12 15:17:40 +0700

    menambah

---
+++ src/MailSender.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+class MailSender {
+  constructor() {
+    this._transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.MAIL_ADDRESS,
+        pass: process.env.MAIL_PASSWORD,
+      },
+      tls: {
+      // do not fail on invalid certs
+        rejectUnauthorized: false,
+      },
+    });
+  }
+
+  sendEmail(targetEmail, content) {
+    const message = {
+      from: 'Notes Apps',
+      to: targetEmail,
+      subject: 'Ekspor Catatan',
+      text: 'Terlampir hasil dari ekspor catatan',
+      attachments: [
+        {
+          filename: 'notes.json',
+          content,
+        },
+      ],
+    };
+
+    return this._transporter.sendMail(message);
+  }
+}
+
+module.exports = MailSender;
